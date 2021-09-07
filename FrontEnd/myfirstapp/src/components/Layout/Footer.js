57ce09c02fe755a7648aa91b01fb01196fb7e32c
sara <s3808578@student.rmit.edu.au>
2021-09-07 22:48:14 +1000

    Merge branch 'LandingPage' of https://github.com/s3844467/JUVKS into sara

---
+++ FrontEnd/myfirstapp/src/components/Layout/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import '..//Styles/Footer.css';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer className="footer">
+                    <h5>© 2021 Bookeroo. All rights reserved. </h5>
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default Footer;
